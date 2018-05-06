7f3e9290087912d655f3ea5efb4a15c6d733249a
Konstantin Kushnir <chpock@gmail.com>
2018-05-06 23:35:19 +0300

    Added copyright info

--- Skins/ck.widgets/@Resources/Scripts/Config/harddrive.tcl
+++ Skins/ck.widgets/@Resources/Scripts/Config/harddrive.tcl
@@ -1,3 +1,11 @@
+# ck.widgets
+# Copyright (C) 2018 Konstantin Kushnir <chpock@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
 set debug true
 
 proc Initialize {} {
